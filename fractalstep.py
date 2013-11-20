e4cad38c719014dceabd8865cda7cf270fd6e255
Max Isacson <trepunktfjorton@gmail.com>
2013-11-20 23:57:32 +0100

    trying to sort out choosing colors

--- fractalstep.py
+++ fractalstep.py
@@ -8,10 +8,11 @@ import ttk
 class turtle(object): #drawing automaton
 	angle = 0
 	draw = True
-	def __init__(self, window, xinit, yinit):
+	def __init__(self, window, xinit, yinit, in_color = (255, 255, 255)):
 		self.window = window
 		self.x = xinit
 		self.y = yinit
+		self.color = in_color
 	
 	def setDraw(self, var):
 		self.draw = var	
@@ -26,11 +27,14 @@ class turtle(object): #drawing automaton
 	def rotateBy(self, beta):
 		self.angle = self.angle + math.radians(-beta)
 
+	def setColor(self, in_color):
+		self.color = in_color
+
 	def move(self, dist):
 		x_new = self.x + dist*math.cos(self.angle)
 		y_new = self.y + dist*math.sin(self.angle)
 		if self.draw:
-			pygame.draw.line(self.window, (255, 255, 255), (self.x, self.y), (x_new, y_new), 1)
+			pygame.draw.line(self.window, self.color, (self.x, self.y), (x_new, y_new), 1)
 		else:
 			pass
 
@@ -43,9 +47,10 @@ class turtle(object): #drawing automaton
 	def printAngle(self):
 		print str(-math.degrees(self.angle))
 
-def dragon(x_start, y_start, in_dist, niter, in_window): #dragon curve
+def dragon(x_start, y_start, in_dist, niter, in_window, in_color): #dragon curve
 	window = in_window
-	t = turtle(window, x_start, y_start)
+	color = in_color
+	t = turtle(window, x_start, y_start, color)
 	angle = 90
 	dist = in_dist
 	t.setAngle(angle)
@@ -73,9 +78,10 @@ def dragon(x_start, y_start, in_dist, niter, in_window): #dragon curve
 		dist = dist/math.sqrt(2) #scale down by sqrt(2)
 		time.sleep(0.5)	
 
-def sqrkochcurve(x_start, y_start, in_dist, niter, in_window): #square koch curve (quadratic type 1)
+def sqrkochcurve(x_start, y_start, in_dist, niter, in_window, in_color): #square koch curve (quadratic type 1)
 	window = in_window
-	t = turtle(window, x_start, y_start)
+	color = in_color
+	t = turtle(window, x_start, y_start, in_color)
 	dist = in_dist
 	steps = [1, -1, -1, 1]
 	pattern = steps[:]
@@ -106,9 +112,10 @@ def sqrkochcurve(x_start, y_start, in_dist, niter, in_window): #square koch curv
 		dist = float(dist)/3.
 		time.sleep(.5)
 
-def kochcurve(x_start, y_start, in_dist, niter, in_window): #standard koch curve
+def kochcurve(x_start, y_start, in_dist, niter, in_window, in_color): #standard koch curve
 	window = in_window
-	t = turtle(window, x_start, y_start)
+	color = in_color
+	t = turtle(window, x_start, y_start, color)
 	dist = in_dist
 	steps = [1, -1, 1]
 	pattern = steps[:]
@@ -143,19 +150,53 @@ def kochcurve(x_start, y_start, in_dist, niter, in_window): #standard koch curve
 		dist = float(dist)/3.
 		time.sleep(.5)
 
-def run(): #this runs the drawing methods, called from run button in gui
+def run(*args): #this runs the drawing methods, called from run button in gui
 	pygame.init() #initialize pygame
 	window = pygame.display.set_mode((640,480)) #set display window
+	print 'color:'
+	for x in color:
+		print str(x)
+
 	if fracvar.get() == 'Dragon':
-		dragon(200, 300, 200, 17, window)
+		dragon(200, 300, 200, 17, window, color)
 	elif fracvar.get() == 'Koch':
-		kochcurve(20, 400, 200, 6, window)
+		kochcurve(20, 400, 200, 6, window, color)
 	elif fracvar.get() == 'Koch sqr':
-		sqrkochcurve(20, 400, 200, 6, window)
+		sqrkochcurve(20, 400, 200, 6, window, color)
+
+def options(*args): #set fractal specific options
+	s = fracvar.get()
+	if s == 'Dragon':
+		red = Tkinter.StringVar()
+		green = Tkinter.StringVar()
+		blue = Tkinter.StringVar()
+
+		red_entry = ttk.Entry(optframe, textvariable=red, width=3)
+		red_entry.grid(column=0, row=0, sticky='w')
+
+		green_entry = ttk.Entry(optframe, textvariable=green, width=3)
+		green_entry.grid(column=1, row=0, sticky='w')
+
+		blue_entry = ttk.Entry(optframe, textvariable=blue, width=3)
+		blue_entry.grid(column=2, row=0, sticky='w')
+
+		if (red.get() != '' and green.get() != '' and blue.get() != ''):
+			if (int(red.get()) < 256 and int(green.get()) < 256 and int(blue.get()) < 256):
+				if (int(red.get()) > -1 and int(green.get()) > -1 and int(blue.get()) > -1):
+					global color
+					color = (int(red.get()), int(green.get()), int(blue.get()))
+
+	elif s == 'Koch':
+		pass
+	elif s == 'Koch sqr':
+		pass
+
+
 
 ## Initialization and stuff ##
 
 fractals = ('Dragon', 'Koch', 'Koch sqr') #list of fractals available
+color = (255, 255, 255) #default color
 
 root = Tkinter.Tk()
 root.title("Max magiska laada")
@@ -165,9 +206,19 @@ mainframe.grid(column=0, row=0, sticky="n, w, e, s")
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
+optframe = ttk.Frame(mainframe, padding="3 3 12 12")
+optframe.grid(column=0, row=1, sticky="n, w")
+optframe.columnconfigure(0, weight=1)
+optframe.rowconfigure(0, weight=1)
+
 fracvar = Tkinter.StringVar()
-frac = ttk.Combobox(mainframe, textvariable=fracvar, values=fractals, state='readonly').grid(column=1, row=1, sticky='w')
-ttk.Button(mainframe, text='Go!', command=run).grid(column=2, row=1, sticky='w')
+frac = ttk.Combobox(mainframe, textvariable=fracvar)
+frac.grid(column=0, row=0, sticky='w')
+frac['values'] = fractals
+frac['state'] = 'readonly'
+frac.bind('<<ComboboxSelected>>', options)
+frac.current(0)
+ttk.Button(mainframe, text='Go!', command=run).grid(column=1, row=0, sticky='w')
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
